114a3ae2f9e6dcbf5ca4dfd2af59e684d85bbdce
hohemarie <marie.goudon@laposte.net>
2018-11-18 20:43:31 +0100

    ajoute exo 8

---
+++ exo7c.rb
@@ -0,0 +1,2 @@
+user_name = gets.chomp#ligne constituant un champ de saisie du nom
+puts user_name#affichage de la meme chaine de caractere sur la ligne suivante
---
+++ exo8.rb
@@ -0,0 +1,5 @@
+puts "Votre nom, Svp?"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, {user_name}"
+

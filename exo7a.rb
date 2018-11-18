671821dca3d25c4f3ef7b51072afe29799f3d5eb
hohemarie <marie.goudon@laposte.net>
2018-11-18 20:28:53 +0100

    ajout de exo 7ab

---
+++ exo7a.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blqse ?"
+user_name = gets.chomp#cela fait que le nom de la variable saffiche et il est demande a lutilisateur de lui attribuer une valeur? (essai) (tenetative de reponse)
+puts user_name#affiche la valeur de user name
+
---
+++ exo7b.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blase?"#affiche la question sur une liste
+print "> "#ajoute un signe sur la gauche pour entrer son nom
+user_name = gets.chomp#enregostrement du nom
+puts user_name#affichage du nom sur une nouvelle lignem repetition de la ligne precedente sans marge ou signe precedent le mot

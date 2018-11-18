81dc242e4cd26a1e4a1ff46533a6843c1dad6598
hohemarie <marie.goudon@laposte.net>
2018-11-18 18:39:20 +0100

    exo 5

---
+++ exo5.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+puts "compter le nombre d'heures de travail à THP"#ceci est une ligne de programme qui fait afficher la chaine de caracteres qu'il y a entre guillemets
+puts "Travail : #{10 * 5 * 11}"#ceci est une ligne de programme qui affiche d'abord le contenu de la premiere partie de la chaine jusqua le idese et les accolades. Ensuite le programme affiche le resultat du calcul. Le programme commence par calculer le comtenu des accolades puis il donne le resultat puis il affiche le resultat, comme sil faisait partie de la chaine initiale. Le programme affiche le resultat du calcul de lexpression algebrique entre parentheses a la suite de la premiere partie de la chaine.
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"#le programme affiche la premiere partie de la chaine. le programme affiche tous les caracteres qui sont entre le premnier guillemet de la ligne jusqu'a l'espace qui est avant le diese avec les deux points. ensuite le programme sarrete dafficher. il lit lexpression algebrique entre parenthesesm effectue le calcul donnel resultat. Il fait la concatenation de la chaine de caracteres et du resultat de loperation. puis il affiche la chaine resultat de cette concatenation sur la ligne suivant laffichage de la chaine de caracteres precedente.
+
+puts "Et en secondes ?"#le programme a affiche la chaine de caracteres "et en secondes"
+
+puts 10 * 5 * 11 * 60 * 60#le programme lit le calcul qui est situe apres le put jusqua la fin de la ligne. il leffectue et affiche le resultat sur la ligne suivant la chaine de caracteres precedente.
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"#le programme se contente d'afficher la chaine de caracteres en entier.
+
+puts 3 + 2 < 5 - 7#le programme lit separement les expressions algebriques se situant respectivement a droite et a gauche du signe logique de comparaison. Il effectue le calcul et obtient la somme des expressions algebriques respectives. Il compare les deux valeurs et observe le sens du signe de comparaison. Apres avoir analyse si laffirmation semantique est juste, il affiche vrai ou faux.
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"#ici le programme commence par selectionner le texte qui commence aux guillemets et qui se termine au diesem il le garde en memoire. ensuite le programme lit lexpression algebrique qui se trouve entre les accolades. Il effectue le calcul, obtient le resultat, puis attache le texte garde en memoire avec le resultat du calcul par concatenation. Ensuite il affiche sur la ligne suivant laffichage de resultat de la comparaison precent le resultat de cette concatenation.
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"#ici le programme commence par garder en memoire la premiere partie de la chaine de caracteres jusquau diese. Ensuite il effectue le calcul entre les accolades, obtient un chiffre negatifm et garde ce chiffre negatif en memoire dans une case de memoire. ensuite le programme associe par concatenation les deux variables obtenues precedemment et les affiche sur la ligne suivant le dernier affichage d'une chaine de caracteres sur la console dexecution.
+
+puts "Ok, c'est faux alors !"#ici le programme se contente dafficher la chaine de caracteres proprosee sans perturber lordre des caracteres donne·
+
+puts "C'est drôle ça, faisons-en plus :"#ici le programme continue a proceder de la sorte.
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"#ici le programme commence par sauvegarder en memoire la premiere partie de la chaine de caracteres proposee. Ensuite le programme determine la validite ou faussete de la comparaison proposee sous forme daffirmation entre les accolades. Ensuite le programme fait la concatenation de la premiere partie de la chaine de caracteres avec le resultat de la comparaison entre deux valeurs algebriques. Il affiche le resultat de cette concatenation sur la ligne suivant le dernier affichage dune ligne de console. j
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"#ici le programme commence par sauvegarder en memoire la premiere partie de la chaine de caracteres. le programme fait la meme chose sauf que le signe de comparaison change.
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"#ici le signe de comparaison change.

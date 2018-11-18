a47f6b6c51302565f841dfa2277659f8b9e9841e
hohemarie <marie.goudon@laposte.net>
2018-11-18 20:51:50 +0100

    ajout du exo 9

---
+++ exo9.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, premiere chose quel est ton prenom ?"
+print "> "
+user_first_name = gets.chomp
+puts "Bonjour, deuxieme chose quel est ton nom de famille ?"
+print "> "
+user_family_name = gets.chomp
+puts "Bonjour, #{user_first_name} #{user_family_name}"
